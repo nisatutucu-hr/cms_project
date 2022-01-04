0f81fb447d63f9395445a25f329cb475d4e19d39
yusuf kaygin <yusuf.two000@gmail.com>
2022-01-04 20:16:14 +0300

    projecnin ilk dersinin commiti

--- panel/assets/js/library.js
+++ panel/assets/js/library.js
@@ -1,87 +1,85 @@
 var LIBS = {
-	// Chart libraries
-	plot: [
-		"../libs/misc/flot/jquery.flot.min.js",
-		"../libs/misc/flot/jquery.flot.pie.min.js",
-		"../libs/misc/flot/jquery.flot.stack.min.js",
-		"../libs/misc/flot/jquery.flot.resize.min.js",
-		"../libs/misc/flot/jquery.flot.curvedLines.js",
-		"../libs/misc/flot/jquery.flot.tooltip.min.js",
-		"../libs/misc/flot/jquery.flot.categories.min.js"
-	],
-	chart: [
-		'../libs/misc/echarts/build/dist/echarts-all.js',
-		'../libs/misc/echarts/build/dist/theme.js',
-		'../libs/misc/echarts/build/dist/jquery.echarts.js'
-	],
-	circleProgress: [
-		"../libs/bower/jquery-circle-progress/dist/circle-progress.js"
-	],
-	sparkline: [
-		"../libs/misc/jquery.sparkline.min.js"
-	],
-	maxlength: [
-		"../libs/bower/bootstrap-maxlength/src/bootstrap-maxlength.js"
-	],
-	tagsinput: [
-		"../libs/bower/bootstrap-tagsinput/dist/bootstrap-tagsinput.css",
-		"../libs/bower/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js",
-	],
-	TouchSpin: [
-		"../libs/bower/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.css",
-		"../libs/bower/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js"
-	],
-	selectpicker: [
-		"../libs/bower/bootstrap-select/dist/css/bootstrap-select.min.css",
-		"../libs/bower/bootstrap-select/dist/js/bootstrap-select.min.js"
-	],
-	filestyle: [
-		"../libs/bower/bootstrap-filestyle/src/bootstrap-filestyle.min.js"
-	],
-	timepicker: [
-		"../libs/bower/bootstrap-timepicker/js/bootstrap-timepicker.js"
-	],
-	datetimepicker: [
-		"../libs/bower/moment/moment.js",
-		"../libs/bower/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css",
-		"../libs/bower/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"
-	],
-	select2: [
-		"../libs/bower/select2/dist/css/select2.min.css",
-		"../libs/bower/select2/dist/js/select2.full.min.js"
-	],
-	vectorMap: [
-		"../libs/misc/jvectormap/jquery-jvectormap.css",
-		"../libs/misc/jvectormap/jquery-jvectormap.min.js",
-		"../libs/misc/jvectormap/maps/jquery-jvectormap-us-mill.js",
-		"../libs/misc/jvectormap/maps/jquery-jvectormap-world-mill.js",
-		"../libs/misc/jvectormap/maps/jquery-jvectormap-africa-mill.js"
-	],
-	summernote: [
-		"../libs/bower/summernote/dist/summernote.css",
-		"../libs/bower/summernote/dist/summernote.min.js"
-	],
-	DataTable: [
-		"../libs/misc/datatables/datatables.min.css",
-		"../libs/misc/datatables/datatables.min.js"
-	],
-	fullCalendar: [
-		"../libs/bower/moment/moment.js",
-		"../libs/bower/fullcalendar/dist/fullcalendar.min.css",
-		"../libs/bower/fullcalendar/dist/fullcalendar.min.js"
-	],
-	dropzone: [
-		"../libs/bower/dropzone/dist/min/dropzone.min.css",
-		"../libs/bower/dropzone/dist/min/dropzone.min.js"
-	],
-	counterUp: [
-		"../libs/bower/waypoints/lib/jquery.waypoints.min.js",
-		"../libs/bower/counterup/jquery.counterup.min.js"
-	],
-	others: [
-		"../libs/bower/switchery/dist/switchery.min.css",
-		"../libs/bower/switchery/dist/switchery.min.js",
-		"../libs/bower/lightbox2/dist/css/lightbox.min.css",
-		"../libs/bower/lightbox2/dist/js/lightbox.min.js"
-	]
+  // Chart libraries
+  plot: [
+    "../panel/libs/misc/flot/jquery.flot.min.js",
+    "../panel/libs/misc/flot/jquery.flot.pie.min.js",
+    "../panel/libs/misc/flot/jquery.flot.stack.min.js",
+    "../panel/libs/misc/flot/jquery.flot.resize.min.js",
+    "../panel/libs/misc/flot/jquery.flot.curvedLines.js",
+    "../panel/libs/misc/flot/jquery.flot.tooltip.min.js",
+    "../panel/libs/misc/flot/jquery.flot.categories.min.js",
+  ],
+  chart: [
+    "../panel/libs/misc/echarts/build/dist/echarts-all.js",
+    "../panel/libs/misc/echarts/build/dist/theme.js",
+    "../panel/libs/misc/echarts/build/dist/jquery.echarts.js",
+  ],
+  circleProgress: [
+    "../panel/libs/bower/jquery-circle-progress/dist/circle-progress.js",
+  ],
+  sparkline: ["../panel/libs/misc/jquery.sparkline.min.js"],
+  maxlength: [
+    "../panel/libs/bower/bootstrap-maxlength/src/bootstrap-maxlength.js",
+  ],
+  tagsinput: [
+    "../panel/libs/bower/bootstrap-tagsinput/dist/bootstrap-tagsinput.css",
+    "../panel/libs/bower/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js",
+  ],
+  TouchSpin: [
+    "../panel/libs/bower/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.css",
+    "../panel/libs/bower/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js",
+  ],
+  selectpicker: [
+    "../panel/libs/bower/bootstrap-select/dist/css/bootstrap-select.min.css",
+    "../panel/libs/bower/bootstrap-select/dist/js/bootstrap-select.min.js",
+  ],
+  filestyle: [
+    "../panel/libs/bower/bootstrap-filestyle/src/bootstrap-filestyle.min.js",
+  ],
+  timepicker: [
+    "../panel/libs/bower/bootstrap-timepicker/js/bootstrap-timepicker.js",
+  ],
+  datetimepicker: [
+    "../panel/libs/bower/moment/moment.js",
+    "../panel/libs/bower/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css",
+    "../panel/libs/bower/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js",
+  ],
+  select2: [
+    "../panel/libs/bower/select2/dist/css/select2.min.css",
+    "../panel/libs/bower/select2/dist/js/select2.full.min.js",
+  ],
+  vectorMap: [
+    "../panel/libs/misc/jvectormap/jquery-jvectormap.css",
+    "../panel/libs/misc/jvectormap/jquery-jvectormap.min.js",
+    "../panel/libs/misc/jvectormap/maps/jquery-jvectormap-us-mill.js",
+    "../panel/libs/misc/jvectormap/maps/jquery-jvectormap-world-mill.js",
+    "../panel/libs/misc/jvectormap/maps/jquery-jvectormap-africa-mill.js",
+  ],
+  summernote: [
+    "../panel/libs/bower/summernote/dist/summernote.css",
+    "../panel/libs/bower/summernote/dist/summernote.min.js",
+  ],
+  DataTable: [
+    "../panel/libs/misc/datatables/datatables.min.css",
+    "../panel/libs/misc/datatables/datatables.min.js",
+  ],
+  fullCalendar: [
+    "../panel/libs/bower/moment/moment.js",
+    "../panel/libs/bower/fullcalendar/dist/fullcalendar.min.css",
+    "../panel/libs/bower/fullcalendar/dist/fullcalendar.min.js",
+  ],
+  dropzone: [
+    "../panel/libs/bower/dropzone/dist/min/dropzone.min.css",
+    "../panel/libs/bower/dropzone/dist/min/dropzone.min.js",
+  ],
+  counterUp: [
+    "../panel/libs/bower/waypoints/lib/jquery.waypoints.min.js",
+    "../panel/libs/bower/counterup/jquery.counterup.min.js",
+  ],
+  others: [
+    "../panel/libs/bower/switchery/dist/switchery.min.css",
+    "../panel/libs/bower/switchery/dist/switchery.min.js",
+    "../panel/libs/bower/lightbox2/dist/css/lightbox.min.css",
+    "../panel/libs/bower/lightbox2/dist/js/lightbox.min.js",
+  ],
 };
